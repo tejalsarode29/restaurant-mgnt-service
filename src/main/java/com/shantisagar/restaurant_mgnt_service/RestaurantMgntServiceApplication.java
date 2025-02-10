4a1388cad1aa5b14b49a9ee6dcb5cc3224a4afa8
tejalsarode29 <tejalsarode29@gmail.com>
2025-02-10 17:06:00 +0530

    on startup implemented commandlinerunner

--- src/main/java/com/shantisagar/restaurant_mgnt_service/RestaurantMgntServiceApplication.java
+++ src/main/java/com/shantisagar/restaurant_mgnt_service/RestaurantMgntServiceApplication.java
@@ -1,13 +1,22 @@
 package com.shantisagar.restaurant_mgnt_service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
-public class RestaurantMgntServiceApplication {
+public class RestaurantMgntServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantMgntServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("============== Application Started ==============");
 	}
 
 }
