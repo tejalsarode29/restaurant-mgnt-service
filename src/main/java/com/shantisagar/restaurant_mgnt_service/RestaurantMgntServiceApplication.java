e2ba8322c78bb04d82ca880190752c19d013bbbe
tejalsarode29 <tejalsarode29@gmail.com>
2025-02-10 16:59:14 +0530

    Project Setup

---
+++ src/main/java/com/shantisagar/restaurant_mgnt_service/RestaurantMgntServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shantisagar.restaurant_mgnt_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantMgntServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantMgntServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shantisagar/restaurant_mgnt_service/RestaurantMgntServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shantisagar.restaurant_mgnt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantMgntServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
